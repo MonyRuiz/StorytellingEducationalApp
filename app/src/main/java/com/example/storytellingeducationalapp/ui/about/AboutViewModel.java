5f25c4b3af6a6a9d4a046039786d20ebb1561217
MonyRuiz <lamony2000@gmail.com>
2022-04-26 12:24:40 -0600

    Se agregaron nuevos fragmentos como fragment_about.xml y fragment_help.xml, pero no aparecen con el menú hamburguesa...

---
+++ app/src/main/java/com/example/storytellingeducationalapp/ui/about/AboutViewModel.java
@@ -0,0 +1,10 @@
+package com.example.storytellingeducationalapp.ui.about;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class AboutViewModel extends ViewModel {
+
+
+}
---
+++ app/src/main/java/com/example/storytellingeducationalapp/ui/help/HelpViewModel.java
@@ -0,0 +1,9 @@
+package com.example.storytellingeducationalapp.ui.help;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class HelpViewModel extends ViewModel {
+
+}
